418eb2914e37f727120eaa338aaf12a1da784e8d
Hamed Movasaqpoor <hamed.movasaqpoor@gmail.com>
2024-07-29 10:23:39 +0330

    New: add author name

--- woocommerce-cleanup.php
+++ woocommerce-cleanup.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: دستیار تست انار
 Description: افزونه‌ای برای حذف همه محصولات، ویژگی‌ها و دسته‌بندی‌های ووکامرس، با انحصارات دسته پیش‌فرض محصول.
-Version: 1.4
-Author: Hamed
+Version: 1.5
+Author: حامد موثق پور
 Text Domain: woocommerce-cleanup
 Domain Path: /languages
 */
